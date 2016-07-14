cbc7a959ec768d24e1c4a0014e4eb84baaf064f9
inagantisrikiran <srikiran.ios@gmail.com>
2016-07-14 15:12:59 -0700

    srikiran

---
+++ srikiran/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  srikiran
+//
+//  Created by SRIKIRAN INAGANTI on 7/14/16.
+//  Copyright © 2016 SRIKIRAN INAGANTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ srikiran/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  srikiran
+//
+//  Created by SRIKIRAN INAGANTI on 7/14/16.
+//  Copyright © 2016 SRIKIRAN INAGANTI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
